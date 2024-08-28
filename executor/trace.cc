d434a8d9c48e0bdb6a9ec4ba5b0b1a76282da673
slava0135 <super.novalskiy_0135@inbox.ru>
2024-08-28 10:59:41 +0300

    better trace

--- executor/trace.cc
+++ executor/trace.cc
@@ -1,34 +1,22 @@
+#include <vector>
+#include <string>
+
 #include "executor.h"
 
 struct Trace {
-    int line_;
-    int ret_code_;
-    int errno_;
+    int index;
+    int ret_code;
+    int err;
 };
 
-#define NR_TRACE_CAP 1024000
+std::vector<Trace> traces;
 
-static Trace g_traces[NR_TRACE_CAP];
-static int g_trace_pos = 0;
-
-void append_trace_impl(int line, int ret_code, int err) {
-    if (g_trace_pos >= NR_TRACE_CAP) {
-        DPRINTF("ERROR: trace buffer overflow\n");
-        return;
-    }
-    g_traces[g_trace_pos].line_ = line;
-    g_traces[g_trace_pos].ret_code_ = ret_code;
-    g_traces[g_trace_pos].errno_ = err;
-
-    g_trace_pos += 1;
+void append_trace_impl(int index, int ret_code, int err) {
+    traces.push_back(Trace{index, ret_code, err});
 }
 
 void dump_trace() {
-    printf("> Dumping trace...\n");
-
-    if (g_trace_pos >= NR_TRACE_CAP) {
-        DPRINTF("ERROR: trace buffer overflow\n");
-    }
+    fprintf(stdout, "> Dumping trace...\n");
 
     FILE *trace_dump_fp = fopen("./trace.dat", "w");
     if (!trace_dump_fp) {
@@ -36,10 +24,9 @@ void dump_trace() {
         return;
     }
 
-    for (int i = 0; i < g_trace_pos; ++i) {
-        fprintf(trace_dump_fp, "%d: %d %d\n", g_traces[i].line_,
-                                              g_traces[i].ret_code_,
-                                              g_traces[i].errno_);
+    fprintf(trace_dump_fp, "Index,ReturnCode,Errno\n");
+    for (const Trace& t : traces) {
+        fprintf(trace_dump_fp, "%4d,%8d,%s(%d)\n", t.index, t.ret_code, strerror(t.err), t.err);
     }
 
     fclose(trace_dump_fp);
